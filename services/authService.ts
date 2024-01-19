bf30fa2ef879c88d41f843319b44f9f943709b3c
Duku Aghogho Mathida <mathildaduku@gmail.com>
2024-01-19 21:36:30 +0100

    add axios interfaces for the endpoints

---
+++ services/authService.ts
@@ -0,0 +1,34 @@
+import axiosConfig from "@/config/axios";
+import { AxiosResponse } from "axios";
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+interface LoginResponse {
+  message: string;
+  token: string;
+  user: User;
+}
+
+interface User {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  userType: number;
+  isDeleted: boolean;
+  projects: any[];
+}
+
+class AuthService {
+    static async login(
+        requestBody: LoginRequest
+    ): Promise<AxiosResponse<LoginResponse>> {
+        return await axiosConfig.post("/Auth/Login", requestBody);
+    }
+}
+
+export default AuthService;
